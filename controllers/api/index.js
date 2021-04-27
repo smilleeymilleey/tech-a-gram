66527ea36a2a6b41b5d7c0318261a77508bd98b7
Caitlyn <caitlynmiley.cm@gmail.com>
2021-04-27 12:27:43 -0500

    updated seed data with image url parameters and updated routing information with correct variables

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const PostRoutes = require('./PostRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/Posts', PostRoutes);
 
 module.exports = router;
