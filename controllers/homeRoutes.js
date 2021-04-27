a676022dcf2e854b6e6537fc0ef36444fa88cda8
AllisonWonderland <aneary91@gmail.com>
2021-04-27 14:06:07 -0500

    got the pictures to show up from handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      Post:allPost, 
+      posts:allPost, 
       logged_in: req.session.logged_in 
     });
   } catch (err) {
