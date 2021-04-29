a40a1c1abca6992cd55cd1bbb343cc2df2134f2c
Caitlyn <caitlynmiley.cm@gmail.com>
2021-04-29 10:17:57 -0500

    fixed our api links

--- public/js/profile.js
+++ public/js/profile.js
@@ -38,9 +38,9 @@ const delButtonHandler = async (event) => {
   }
 };
 
-document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+// document
+//   .querySelector('.new-project-form')
+//   .addEventListener('submit', newFormHandler);
  
 
 document
